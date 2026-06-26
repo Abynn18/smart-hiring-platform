b0eeb05dbbe8f599b04582844bff5c3647435d4d
Abin Joy <abinjoy1410@gmail.com>
2026-06-27 01:08:28 +0530

    Initial project setup

---
+++ src/main/java/com/example/smarthiring/SmartHiringPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarthiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHiringPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHiringPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smarthiring/SmartHiringPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarthiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHiringPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
